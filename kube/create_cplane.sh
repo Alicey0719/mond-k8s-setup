9c1e0ea152cb9d9cece48305099d5cd69d681e7f
Alicey <sirius@alicey.dev>
2024-08-25 14:36:24 +0900

    update version

--- kube/create_cplane.sh
+++ kube/create_cplane.sh
@@ -8,7 +8,7 @@ fi
 
 
 kubeadm init \
-	--kubernetes-version=1.30.3 \
+	--kubernetes-version=1.30.4 \
 	--service-dns-domain=mond-k8s.alicey.dev \
 	--apiserver-cert-extra-sans=cp.mond-k8s.alicey.dev \
 	--pod-network-cidr=192.168.0.0/16
